4b0fc16b3e63e6bf2cae80049ba158c0ae821ad3
horowest <akashvevo5@gmail.com>
2020-06-29 18:45:25 +0530

    added union-find.cpp

--- union-find.cpp
+++ union-find.cpp
@@ -2,46 +2,55 @@
 #include<vector>
 using namespace std;
 
-int set[100], rank_[100];
+int parent[10000];
+int rank_[10000];
 
-void create_set(vector<int> arr) {
-    for(int i = 1; i <= arr.size(); i++) {
-        set[i] = i;
-        rank_[i] = 1;
+void create_set(vector<int>& arr, int n) {
+    for(int x: arr) {
+        parent[x] = x;
+        rank_[x] = 0;
     }
 }
 
-int find(int n) {
-    while(n != set[n])
-        n = set[n];
-    return n;
+int find(int x) {
+    while(x != parent[x])
+        x = parent[x];
+    return x;
 }
 
-bool same(int a, int b) {
-    return find(a) == find(b);
+bool same(int x, int y) {
+    int s1 = find(x), s2 = find(y);
+    return s1 == s2;
 }
 
-void union_(int a, int b) {
-    a = find(a);
-    b = find(b);
-    if(rank_[a] < rank_[b]) {
-        int temp = a;
-        a = b;
-        b = temp;
+void union_(int x, int y) {
+    int s1 = find(x), s2 = find(y);
+
+    if(rank_[s1] > rank_[s2]) {
+        parent[s2] = s1;
     }
-    rank_[a] += rank_[b];
-    set[b] = a;
-
+    else {
+        parent[s1] = s2;
+        if(rank_[s1] == rank_[s2])
+            rank_[s2] += 1;
+    }
 }
 
 int main() {
-    vector<int> arr;
+    int n, m, a, b;
+    cin >> n >> m;
 
-    for(int i = 1; i < 5; i++) {
+    vector<int> arr;
+    for(int i = 1; i <= n; i++)
         arr.push_back(i);
+    create_set(arr, n);
+
+    // union
+    while(m--) {
+        cin >> a >> b;
+        if(!same(a, b))
+            union_(a, b);
     }
-    create_set(arr);
-    cout << same(arr[1], arr[2]) << endl;
-    union_(arr[1], arr[2]);
-    cout << same(arr[1], arr[2]) << endl;
+    for(int x: arr) 
+        cout << x << ": " << find(x) << " rnk(" << rank_[x] << ")" << endl;
 }
